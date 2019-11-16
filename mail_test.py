ee9833955e5edcf70e2b44f0dee1563974a7142c
Akalanka Weerasooriya <35002414+aawgit@users.noreply.github.com>
2019-11-16 23:25:50 +0530

    Create mail_test.py

---
+++ mail_test.py
@@ -0,0 +1,33 @@
+from airflow import DAG
+from airflow.operators.email_operator import EmailOperator
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2019, 11, 1)
+}
+
+dag = DAG("email_test", default_args=default_args, schedule_interval=timedelta(days=1))
+
+
+t1 = EmailOperator(
+    task_id="send_mail", 
+    to='receiver@mail.com',
+    subject='Test mail',
+    html_content='<p> You have got mail! <p>',
+    dag=dag)
+    
+    
+def error_function():
+    raise Exception('Something wrong')
+   
+   
+t2 = PythonOperator(
+    task_id='failing_task',
+    python_callable=error_function,
+    email_on_failure=True,
+    emai='receivers@mail.com
+    dag=dag,
+)
